89867bbedd0a61b4c3ab1db881a9f439581fbd5d
Samuel Emde <samuel.emde@gmail.com>
2024-11-25 22:35:40 +0100

    feat: remove invalid css class

--- src/app/(library)/albums/page.tsx
+++ src/app/(library)/albums/page.tsx
@@ -5,7 +5,7 @@ export default async function Page() {
   const albums = await api.album.getAll.query();
 
   return (
-    <div className="lg:grid-cols- grid w-full grid-cols-2 gap-x-4 gap-y-6 sm:gap-x-6 lg:grid-cols-4 lg:gap-x-8">
+    <div className="grid w-full grid-cols-2 gap-x-4 gap-y-6 sm:gap-x-6 lg:grid-cols-4 lg:gap-x-8">
       {albums.map((album) => (
         <AlbumCard key={album.id} album={album} />
       ))}
